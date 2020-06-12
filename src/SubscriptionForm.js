d0fed63a17a49e963915ca8366274cf731d85ce2
Natalie Hall <natalie.diana.hall@gmail.com>
2020-06-11 23:32:02 -0400

    create react app, remove all the basic layout crap, add subscription form -- plus create favicon

---
+++ src/SubscriptionForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function SubscriptionForm() {
+  return (
+    <div className="container">
+      <div class="sp-form-outer sp-force-hide">
+        <div id="sp-form-157588" sp-id="157588" sp-hash="3482dc1028115f249345d173b00076fa30757cc439b81069ac3bbd97b2ce3839" sp-lang="en" class="sp-form sp-form-regular sp-form-embed" sp-show-options="%7B%22satellite%22%3Afalse%2C%22maDomain%22%3A%22login.sendpulse.com%22%2C%22formsDomain%22%3A%22forms.sendpulse.com%22%2C%22condition%22%3A%22onEnter%22%2C%22scrollTo%22%3A25%2C%22delay%22%3A10%2C%22repeat%22%3A3%2C%22background%22%3A%22rgba(0%2C%200%2C%200%2C%200.5)%22%2C%22position%22%3A%22bottom-right%22%2C%22animation%22%3A%22%22%2C%22hideOnMobile%22%3Afalse%2C%22urlFilter%22%3Afalse%2C%22urlFilterConditions%22%3A%5B%7B%22force%22%3A%22hide%22%2C%22clause%22%3A%22contains%22%2C%22token%22%3A%22%22%7D%5D%2C%22analytics%22%3A%7B%22ga%22%3A%7B%22eventLabel%22%3A%22Subscription_form_My_emails%22%2C%22send%22%3Afalse%7D%2C%22ym%22%3A%7B%22counterId%22%3Anull%2C%22eventLabel%22%3Anull%2C%22targetId%22%3Anull%2C%22send%22%3Afalse%7D%7D%7D">
+          <div class="sp-form-fields-wrapper">
+            <div class="sp-message">
+              <div>
+              </div>
+            </div>
+            <form novalidate="" class="sp-element-container ui-sortable ui-droppable ">
+              <div class="sp-field " sp-id="sp-30752364-6226-4cf7-9e03-722f199667d7">
+                <label class="sp-control-label"><span >Email</span><strong >*</strong></label>
+                <input type="email" sp-type="email" name="sform[email]" class="sp-form-control " placeholder="username@gmail.com" sp-tips="%7B%22required%22%3A%22Required%20field%22%2C%22wrong%22%3A%22Wrong%20email%22%7D" required="required" />
+              </div>
+              <div class="sp-field " sp-id="sp-09ac102d-a773-4851-b5f9-f24405dfb07d">
+                <label class="sp-control-label"><span >Name</span><strong >*</strong></label>
+                <input type="text" sp-type="name" name="sform[TmFtZQ==]" class="sp-form-control " placeholder="Full name" sp-tips="%7B%22required%22%3A%22Required%20field%22%7D" required="required" />
+              </div>
+              <div class="sp-field sp-button-container " sp-id="sp-3a65e46d-b014-44da-9775-22718ea7007b">
+                <button id="sp-3a65e46d-b014-44da-9775-22718ea7007b" class="sp-button">Subscribe </button>
+              </div>
+            </form>
+            <div class="sp-link-wrapper sp-brandname__left">
+              <a class="sp-link " target="_blank" rel="noopener noreferrer" href="https://sendpulse.com/forms-powered-by-sendpulse?sn=SmF2YVNjcmlwdCBKdW5nbGU%3D&amp;from=7418891"><span class="sp-link-img">&nbsp;</span><span translate="FORM.PROVIDED_BY">Provided by SendPulse</span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script type="text/javascript" src="//web.webformscr.com/apps/fc3/build/default-handler.js?1591191285660"></script> 
+    </div>
+  )
+}
+
+export default SubscriptionForm;
